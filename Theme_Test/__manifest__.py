a41fcc2d0766d3b3e6ec06d9092ea7b8bc90630f
u-j-r <ulisesjimenez@mit-mut.com>
2022-09-26 17:44:27 -0500

    Test Theme 1.0

---
+++ Theme_Test/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Test theme',
+    'description': 'A cool description for your theme.',
+    'version': '1.0',
+    'author': 'Your name',
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+    ],
+}
